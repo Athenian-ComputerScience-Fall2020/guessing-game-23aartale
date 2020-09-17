ba46ad265aa57574da9ee977f825c301685df89d
23aartale <23aartale@athenian.org>
2020-09-17 17:22:26 +0000

    updated game

--- game1.py
+++ game1.py
@@ -1,18 +1,24 @@
 import random
 x=0
 true_num= random.randint(0, 21)
-guess_num = int(input("Thank you for playing. You have 5 tries to guess a random number. Enter a number 0-20: "))
-while x < 4:
-    if guess_num > true_num:
-        print("Your guess was too high. Try again.")
-        guess_num = int(input("Enter a number 0-20: "))
-        x = x + 1
-    elif guess_num < true_num:
-        print("Your guess was too low. Try again.")
-        guess_num = int(input("Enter a number 0-20: "))
-        x = x + 1
-    else:
-        print("You got it congratulations!")
-        x = x + 5
-        
-print("Too bad you didnt guess it. Better luck next time!")
+
+try:
+    guess_num = int(input("Thank you for playing. You have 5 tries to guess a random number. Enter a number 0-20: "))
+    if guess_num == str:
+#I want it if it types an integer or a number larger than 20 to rerun the except but it doesnt work
+        while x < 4:
+            if guess_num > true_num:
+                x = x + 1
+                print("Your guess was too high. Try again.")
+                guess_num = int(input("Guess a number 0-20: "))
+            elif guess_num < true_num:
+                x = x + 1
+                print("Your guess was too low. Try again.")
+                guess_num = int(input("Guess a number 0-20: "))
+            elif guess_num == true_num:
+                print("You got it congratulations!")
+                x = x + 5
+            else:
+                print("Too bad you didnt guess it. Better luck next time!")
+except:
+    print("Please put a number 0-20: ")
