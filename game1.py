af7609fa79b4fe4c3b378702ce015255c5762b56
23aartale <23aartale@athenian.org>
2020-10-14 22:44:33 +0000

    updated with comments

--- game1.py
+++ game1.py
@@ -1,9 +1,14 @@
+#Milan Boga, Jack Chen
+
 import random
+#x is set to 0 so if it goes over 5 the game realises its done
 x=0
+#number 0-20
 true_num= random.randint(0, 21)
 a = 0
 b = 20
 
+#Function so if user wants to play game again
 def play_game():
     while True:
         guess_num = int(input("Thank you for playing. You have 5 tries to guess a random number. Enter a number 0-20: "))
@@ -25,26 +30,31 @@ def play_game():
 
 
 while True:
+    #the number you guess
     guess_num = int(input("Thank you for playing. You have 5 tries to guess a random number. Enter a number 0-20: "))
     while x < 4:
+        #if number is to high
         if guess_num > true_num:
             x = x + 1
             print("Your guess was too high. Try again.")
             guess_num = int(input("Guess a number between " + str(a) + "- " + str(b))
+        #if number is to low
         elif guess_num < true_num:
             x = x + 1
             print("Your guess was too low. Try again.")
             guess_num = int(input("Guess a number between " + str(a) + "- " + str(b))
+        # if guessed number is correct
         elif guess_num == true_num:
             print("You got it congratulations!")
             x = x + 5
             break
-        if x == 5:
-            print("Better luck next time")
-
-play_again = input('Play again? y/n: ')
+    #once there 5 tries is up
+    if x == 5:
+        print("Better luck next time")
+#if they want to play again
+play_again = input('Want to play again? y/n: ')
 if play_again == 'y':
         play_game()
-if play_again == 'n':
+elif play_again == 'n':
         print("Goodbye")
         
