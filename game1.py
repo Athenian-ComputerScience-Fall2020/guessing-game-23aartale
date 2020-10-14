d1d976740a9e6593491700f1b142ca5fa748176a
23aartale <23aartale@athenian.org>
2020-10-14 03:32:34 +0000

    updated

--- game1.py
+++ game1.py
@@ -1,24 +1,50 @@
 import random
 x=0
 true_num= random.randint(0, 21)
+a = 0
+b = 20
 
-try:
-    guess_num = int(input("Thank you for playing. You have 5 tries to guess a random number. Enter a number 0-20: "))
-    if guess_num == str:
-#I want it if it types an integer or a number larger than 20 to rerun the except but it doesnt work
+def play_game():
+    while True:
+        guess_num = int(input("Thank you for playing. You have 5 tries to guess a random number. Enter a number 0-20: "))
         while x < 4:
             if guess_num > true_num:
                 x = x + 1
                 print("Your guess was too high. Try again.")
-                guess_num = int(input("Guess a number between " + a + "- " + b ":"))
+                guess_num = int(input("Guess a number between " + a + "- " + b))
             elif guess_num < true_num:
                 x = x + 1
                 print("Your guess was too low. Try again.")
-                guess_num = int(input("Guess a number between " + a + "- " + b ":"))
+                guess_num = int(input("Guess a number between " + a + "- " + b))
             elif guess_num == true_num:
                 print("You got it congratulations!")
                 x = x + 5
-            else:
-                print("Too bad you didnt guess it. Better luck next time!")
-except:
-    print("Please put a number between " + a + "- " + b ":")
+                break
+        if x == 5:
+            print("Better luck next time")
+
+
+while True:
+    guess_num = int(input("Thank you for playing. You have 5 tries to guess a random number. Enter a number 0-20: "))
+    while x < 4:
+        if guess_num > true_num:
+            x = x + 1
+            print("Your guess was too high. Try again.")
+            guess_num = int(input("Guess a number between " + str(a) + "- " + str(b))
+        elif guess_num < true_num:
+            x = x + 1
+            print("Your guess was too low. Try again.")
+            guess_num = int(input("Guess a number between " + str(a) + "- " + str(b))
+        elif guess_num == true_num:
+            print("You got it congratulations!")
+            x = x + 5
+            break
+        if x == 5:
+            print("Better luck next time")
+
+play_again = input('Play again? y/n: ')
+if play_again == 'y':
+        play_game()
+if play_again == 'n':
+        print("Goodbye")
+        
