9a8d9de692517ae8aa8364e948e87cc2655e10c0
adrianartale <23aartale@athenian.org>
2020-11-11 19:08:23 -0800

    Finally all done and finished I hope

--- game1.py
+++ game1.py
@@ -1,29 +1,28 @@
 #Collaborators: Milan Boga, Jack Chen
 import random
 #Function so if user wants to play game again
-def play_game(x,a,b,true_num):
+def play_game(x,a,b,y,true_num):
     try:
-        print("Thank you for playing. You have 5 tries to guess a random number. Enter a number " + str(a) + " -", str(b) + ": ")
+        print("Thank you for playing. You have", str(y),  "tries to guess a random number. Enter a number " + str(a) + " -", str(b) + ": ")
         guess_num = int(input())
     except:
         print("Invalid input")
-        print("Thank you for playing. You have 5 tries to guess a random number. Enter a number " + str(a) + " -", str(b) + ": ")
+        print("Thank you for playing. You have", str(y),  "tries to guess a random number. Enter a number " + str(a) + " -", str(b) + ": ")
         guess_num = int(input())
-    while x < 4:
+    while x < y:
         if guess_num > true_num:
-            x = x + 1
+            y = x + 1
             print("Your guess was too high. Try again.")
             guess_num = int(input("Guess a number between " + str(a) + "- " + str(b) + ": "))
         elif guess_num < true_num:
-            x = x + 1
+            y = x + 1
             print("Your guess was too low. Try again.")
             guess_num = int(input("Guess a number between " + str(a) + "- " + str(b) + ": "))
         elif guess_num == true_num:
             print("You got it congratulations!")
-            x = x + 5
             break
 
-        if x == 5:
+        if x == y:
             print("Better luck next time")
 
 #x is set to 0 so if it goes over 5 the game realises its done
@@ -31,16 +30,21 @@ x=0
 #number 0-20
 a = int(input("Put in your lowest number: "))
 b = int(input("Put in your highest number: "))
-
+y = int(input("How many guesses do you want to have: "))
 true_num = random.randint(a, b)
 
-play_game(x,a,b,true_num)
+play_game(x,a,b,y,true_num)
 
 #if they want to play again
 while True:
     play_again = input('Want to play again? y/n: ')
     if play_again == 'y':
-            play_game(x,a,b,true_num)
+        x = 0
+        a = int(input("Put in your lowest number: "))
+        b = int(input("Put in your highest number: "))
+        y = int(input("How many guesses do you want to have: "))
+        true_num = random.randint(a, b)
+        play_game(x,a,b,y,true_num)
     elif play_again == 'n':
-            print("Goodbye")
-            break
+        print("Goodbye")
+        break
